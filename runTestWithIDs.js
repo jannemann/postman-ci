8fbe2feba3c08ac5774c767c1b139130cfa18673
Ben Howes <ben@ben-howes.co.uk>
2016-07-07 13:50:11 +0100

    also make newman send an exit code

--- runTestWithIDs.js
+++ runTestWithIDs.js
@@ -75,7 +75,7 @@ function test(collection, environment) {
     insecure: false, //Disable strict ssl
     asLibrary: true, // this makes sure the exit code is returned as an argument to the callback function
     stopOnError: false, //Stops the runner when a test case fails
-    exitCode: false, //Continue running tests even after a failure, but exit with code=1
+    exitCode: true, //Continue running tests even after a failure, but exit with code=1
     //noSummary: true, //Does not show the summary for each iteration
     //noColor: false, //Disable colored output
     //noTestSymbols: false, //Disable symbols in test output and use PASS|FAIL instead
