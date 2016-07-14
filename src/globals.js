57af9ec9b1c9f080a131b7c5b0e6328d21f3c2c8
Jan Lingemann <lingemann@cto-global.org>
2016-07-14 18:11:26 +0200

    Use distinct collection for login #9, test multiple collections at once, more config options #7

--- src/globals.js
+++ src/globals.js
@@ -10,11 +10,33 @@ function globals() {
     console.log('');
     console.log('    export POSTMAN_API_KEY=00000000000000000000000000000000');
     console.log('    postman-ci list');
-    console.log('    postman-ci test -c POSTMAN_COLLECTION_UID -e POSTMAN_ENVIRONMENT_ID');
+    console.log('    postman-ci test -c POSTMAN_COLLECTION_UID -e POSTMAN_ENVIRONMENT_IDs -i POSTMAN_LOGIN_COLLECTION_ID '); //TODO -o POSTMAN_LOGOUT_COLLECTION_ID
+    console.log('');
+    console.log('    Additional arguments:');
+    console.log('        --stopOnError');
+    console.log('        --globalRedirect');
+    console.log('        --requestTimeout 1500');
     console.log('');
   }
 
   this.version = packageVersion;
+
+  this.newmanOptions = {
+    delay: 500, //Specify a delay (in ms) between requests
+    requestTimeout: 15000, //Specify a request timeout (in ms) for a request (Defaults to 15000 if not set)
+    iterationCount: 1, // define the number of times the runner should run
+    responseHandler: "TestResponseHandler", // the response handler to use
+    insecure: false, //Disable strict ssl
+    asLibrary: true, // this makes sure the exit code is returned as an argument to the callback function
+    stopOnError: false, //Stops the runner when a test case fails
+    exitCode: true, //Continue running tests even after a failure, but exit with code=1
+    globalRedirect: false
+    //noSummary: true, //Does not show the summary for each iteration
+    //noColor: false, //Disable colored output
+    //noTestSymbols: false, //Disable symbols in test output and use PASS|FAIL instead
+    //pretty: true, //(Use with -i) Enable pretty-print while saving imported collections, environments, and globals
+    //outputFileVerbose: tempDir + '/' + 'newman.log'
+  }
 }
 
 
--- src/postman-ci-test.js
+++ src/postman-ci-test.js
@@ -4,10 +4,13 @@
 var Request = require('request');
 var Newman = require('newman');
 var Fs = require('fs');
+var jsonfile = require('jsonfile');
 var program = require('commander');
 
 var globals = require('./globals');
 var globalsInstances = new globals();
+
+var runtimeNewmanOptions = {};
 
 var apiKey = process.env.POSTMAN_API_KEY;
 var host = 'api.getpostman.com';
@@ -31,6 +34,7 @@ var options = {
 };
 
 function getObject(id, type) {
+  console.log("get " + id + ", type: " + type);
   return new Promise(function (fulfill, reject) {
     Request('/' + type + 's/' + id, options, function (error, response, body) {
       if (!error && response.statusCode === 200) {
@@ -45,52 +49,111 @@ function getObject(id, type) {
   });
 }
 
-function test(collection, environment) {
-  var newmanOptions = {
-    //folder: //Specify a single folder to run from a collection
-    envJson: environment, // environment file (in parsed json format)
-    //global: //Specify a Postman globals file as JSON
-    //dataFile: data.csv, //Specify a data file to use either json or csv
-    //exportEnvironment //Specify an output file to dump the Postman environment before exiting
-    //exportGlobals  //Specify an output file to dump Globals before exiting
-    delay: 500, //Specify a delay (in ms) between requests
-    requestTimeout: 15000, //Specify a request timeout (in ms) for a request (Defaults to 15000 if not set)
-    iterationCount: 1, // define the number of times the runner should run
-    testReportFile: resultsDir + '/' + collection.info.name + '_test.xml', // the file to export to
-    responseHandler: "TestResponseHandler", // the response handler to use
-    insecure: false, //Disable strict ssl
-    asLibrary: true, // this makes sure the exit code is returned as an argument to the callback function
-    stopOnError: false, //Stops the runner when a test case fails
-    exitCode: true, //Continue running tests even after a failure, but exit with code=1
-    //noSummary: true, //Does not show the summary for each iteration
-    //noColor: false, //Disable colored output
-    //noTestSymbols: false, //Disable symbols in test output and use PASS|FAIL instead
-    //pretty: true, //(Use with -i) Enable pretty-print while saving imported collections, environments, and globals
-    //outputFileVerbose: tempDir + '/' + 'newman.log'
+function execute(collection, environment, exportEnvironment) {
+  var newmanOptions = {};
+  for (var key in runtimeNewmanOptions) {
+    newmanOptions[key] = runtimeNewmanOptions[key];
+  }
+  //newmanOptions.exportGlobalsFile = tempDir + '/' + 'globals.json'; //Specify an output file to dump Globals before exiting
+  //global: //Specify a Postman globals file as JSON
+  //dataFile: data.csv, //Specify a data file to use either json or csv
+  newmanOptions.envJson = environment; // environment file (in parsed json format)
+  newmanOptions.testReportFile = resultsDir + '/' + collection.info.name + '_test.xml'; // the file to export to
+  if (exportEnvironment) {
+    newmanOptions.exportEnvironmentFile = tempDir + '/' + 'loggedInEnvironment.json'; //Specify an output file to dump the Postman environment before exiting
   }
 
-  // Optional Callback function which will be executed once Newman is done executing all its tasks.
-  Newman.execute(collection, newmanOptions, newman_finished_callback);
+  return new Promise(function (fulfill, reject) {
+    Newman.execute(collection, newmanOptions, function (exitCode) {
+      console.log('Newman finished with code ' + exitCode);
+      if (exitCode === 0) {
+        if (exportEnvironment) {
+          jsonfile.readFile(tempDir + '/' + 'loggedInEnvironment.json', 'utf-8', function (error, data) {
+            if(error) {
+              reject(error);
+            }
+            fulfill(data);
+            Fs.unlink(tempDir + '/' + 'loggedInEnvironment.json');
+          });
+        } else {
+          fulfill(environment);
+        }
+      } else {
+        reject(exitCode);
+      }
+    });
+  })
 }
 
-function newman_finished_callback(exitCode) {
-  console.log('Newman finished with code ' + exitCode);
-  process.exit(exitCode);
+function list(val) {
+  return val.split(',');
 }
 
 function main() {
   program
-    .option('-c, --collection [uid]', 'Specify a Postman collection UID')
-    .option('-e, --environment [id]', 'Specify a Postman environment ID')
+    .option('-c, --collections <UIDs>', 'Specify a Postman collection UID', list)
+    .option('-e, --environment [ID]', 'Specify a Postman environment ID')
+    .option('-i, --login [UID]', 'Collection for login, executed first')
+    .option('-o, --logout [UID]', 'Collection for logout, executed last')
+    .option('--stopOnError', 'stops newman on errors')
+    .option('--globalRedirect', 'allows newman on errors')
+    .option('--requestTimeout [ms]', 'sets timeout for newman requsts in milliseconds', parseInt)
     .on('--help', function () {
       globalsInstances.printHelp();
     })
     .parse(process.argv);
 
-  if (!program.environment || !program.collection) {
+  for (var key in globalsInstances.newmanOptions) {
+    runtimeNewmanOptions[key] = globalsInstances.newmanOptions[key];
+  }
+  if(program.stopOnError) {
+    runtimeNewmanOptions.stopOnError = true;
+  }
+  if(program.globalRedirect) {
+    runtimeNewmanOptions.globalRedirect = true;
+  }
+  if(program.requestTimeout) {
+    runtimeNewmanOptions.requestTimeout = program.requestTimeout;
+  }
+
+  if (!program.environment || program.collections.length < 1) {
     program.help();
+    process.exit(1);
+  }
+
+  var environmentPromise = getObject(program.environment, 'environment');
+  if (program.login) {
+    var loginPromis = getObject(program.login, 'collection');
+
+    Promise.all([environmentPromise, loginPromis])
+      .then(function (results) {
+          //environmentPromise = execute(results[1], results[0], true);
+          environmentPromise = execute(results[1], results[0]);
+          testAllCollections(environmentPromise, program.collections);
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log("Failed to fetch login collection");
+        process.exit(1);
+      });
   } else {
-    Promise.all([getObject(program.environment, 'environment'), getObject(program.collection, 'collection')])
+    testAllCollections(environmentPromise, program.collections);
+  }
+}
+
+function testAllCollections(environmentPromise, collections) {
+  var sequence = Promise.resolve();
+
+  collections.forEach(function (entry) {
+    sequence = sequence.then(function(){
+      return testCollection(environmentPromise, getObject(entry, 'collection'));
+    });
+  });
+}
+
+function testCollection(environmentPromise, collectionPromise) {
+  return new Promise(function (fulfill, reject) {
+    Promise.all([environmentPromise, collectionPromise])
       .then(function (results) {
         var collection = results[1];
         var environment = results[0];
@@ -99,14 +162,19 @@ function main() {
         console.log('collection name: ' + collection.info.name);
 
         console.log('start test');
-        test(collection, environment);
+        execute(collection, environment)
+        .then(function () {
+          console.log("Testing " + collection.info.name + " done.");
+          fulfill(environment);
+        });
       })
       .catch(function (error) {
         console.log(error);
         console.log("Failed to fetch testing specs");
-        process.exit(1);
+        reject(error);
       });
-  }
-}
+  })
+};
+
 
 main();
